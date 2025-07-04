60df1a15004d2da11a856f248d990a22409355ce
matteo.clz <matteocolazilli@gmail.com>
2025-07-04 17:56:22 +0200

    Refactor run.sh to improve clarity and maintainability of Kubernetes deployment steps

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ kubectl wait --for=condition=Ready pods --all -n ping-app --timeout=300s
 kubectl apply -f k8s/runner/namespace.yaml
 kubectl apply -f k8s/runner/rbac.yaml    
 
-kubectl create secret generic github-runner-secret --namespace=github-runner --from-literal=runner-token='ANQIW5G5M5ICSMFK6UC3JATINAAOW' # <-- inserire il token del runner qui
+kubectl create secret generic github-runner-secret --namespace=github-runner --from-literal=runner-token='' # <-- inserire il token del runner qui
 
 export MACHINE_ID="matteo" #<-- da cambiare in base alla macchina
 cat k8s/runner/runner-deployment.yaml | sed "s/MACHINE_IDENTIFIER/$MACHINE_ID/g" | kubectl apply -f -
