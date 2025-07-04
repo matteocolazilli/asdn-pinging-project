0fabe91b848fe4d5fd9f38f27683b62e9569d8ad
matteo.clz <matteocolazilli@gmail.com>
2025-07-04 19:49:37 +0200

    Remove waiting for pods readiness and commented out runner token echo in deployment script

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@ kind delete cluster
 kind create cluster --config kind/kind-config.yaml
 kubectl apply -f k8s/app/
 
-echo "Waiting for all pods to be ready..."
-kubectl wait --for=condition=Ready pods --all -n ping-app --timeout=300s
-
 kubectl apply -f k8s/runner/namespace.yaml
 kubectl apply -f k8s/runner/rbac.yaml    
 
@@ -13,5 +10,3 @@ kubectl create secret generic github-runner-secret --namespace=github-runner --f
 export MACHINE_ID="matteo" #<-- da cambiare in base alla macchina
 cat k8s/runner/runner-deployment.yaml | sed "s/MACHINE_IDENTIFIER/$MACHINE_ID/g" | kubectl apply -f -
 
-
-#echo $RUNNER_TOKEN 
