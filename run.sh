35c900a74d19be76ce76dd39368427021025a138
matteo.clz <matteocolazilli@gmail.com>
2025-07-04 13:48:42 +0200

    Add script to run Kubernetes cluster

---
+++ run.sh
@@ -0,0 +1,11 @@
+kind delete cluster
+kind create cluster --config kind-config.yaml
+kubectl apply -f k8s/app/
+
+echo "Waiting for all pods to be ready..."
+kubectl wait --for=condition=Ready pods --all -n ping-app --timeout=300s
+
+
+
+#echo $RUNNER_TOKEN 
+#kubectl create secret generic github-runner-secret --namespace=github-runner --from-literal=runner-token='' 
