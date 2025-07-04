d9e0895a35adf22dea83564ba5d8d8ff032749e9
matteo.clz <matteocolazilli@gmail.com>
2025-07-04 17:32:44 +0200

    Update deployment configurations for GitHub runner and enhance setup script for Machine A

--- run.sh
+++ run.sh
@@ -5,7 +5,13 @@ kubectl apply -f k8s/app/
 echo "Waiting for all pods to be ready..."
 kubectl wait --for=condition=Ready pods --all -n ping-app --timeout=300s
 
+kubectl apply -f k8s/runner/namespace.yaml
+kubectl apply -f k8s/runner/rbac.yaml    
+
+kubectl create secret generic github-runner-secret --namespace=github-runner --from-literal=runner-token='ANQIW5G5M5ICSMFK6UC3JATINAAOW' # <-- inserire il token del runner qui
+
+export MACHINE_ID="matteo" #<-- da cambiare in base alla macchina
+cat k8s/runner/runner-deployment.yaml | sed "s/MACHINE_IDENTIFIER/$MACHINE_ID/g" | kubectl apply -f -
 
 
 #echo $RUNNER_TOKEN 
-#kubectl create secret generic github-runner-secret --namespace=github-runner --from-literal=runner-token='' 
